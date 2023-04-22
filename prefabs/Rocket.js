a2fde1a626ebfd60fc82b32faeeb4ffd627b3b1b
Michael Nieto <mdnieto@ucsc.edu>
2023-04-22 16:36:37 -0700

    Complete Rocket Patrol Experience

--- prefabs/Rocket.js
+++ prefabs/Rocket.js
@@ -6,6 +6,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.isFiring = false;
         this.moveSpeed = 2;
+
+        this.sfxRocket = scene.sound.add('sfx_rocket');
     }
 
     update() {
@@ -19,8 +21,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         // Fire button
-        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();
         }
 
         // If fired, move up
